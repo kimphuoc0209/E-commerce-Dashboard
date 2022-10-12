600d906f5866a0d759c76c8b7cb4383d2d52cf99
nhokshiroupro <shiroutran@gmail.com>
2022-10-12 14:33:42 +0700

    Orders Component

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Orders = () => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Total</th>
+          <th scope="col">Paid</th>
+          <th scope="col">Date</th>
+          <th>Status</th>
+          <th scope="col" className="text-end">
+            Action
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <b>Vảy Ốc Đỏ</b>
+          </td>
+          <td>kimphuoc@gmail.com</td>
+          <td>25,000 VND</td>
+          <td>
+            <span className="badge rounded-pill alert-success">
+              Paid At Today 08:00 PM
+            </span>
+          </td>
+          <td>Oct 10 2022</td>
+          <td>
+            <span className="badge btn-success">Delivered</span>
+          </td>
+          <td className="d-flex justify-content-end align-items-center">
+            <Link to={`/order`} className="text-success">
+              <i className="fas fa-eye"></i>
+            </Link>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <b>Cá Chuột Pygmy</b>
+          </td>
+          <td>kimphuoc@gmail.com</td>
+          <td>60,000 VND</td>
+          <td>
+            <span className="badge rounded-pill alert-danger">Not paid</span>
+          </td>
+          <td>Oct 10 2022</td>
+          <td>
+            <span className="badge btn-dark">Not Delivered</span>
+          </td>
+          <td className="d-flex justify-content-end align-item-center">
+            <Link to={`/order`} className="text-success">
+              <i className="fas fa-eye"></i>
+            </Link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+export default Orders;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,12 @@ import React from "react";
 import Sidebar from "../components/sidebar";
 // import Header from "../components/Header";
 // import MainProducts from "../components/Products/MainProducts";
-import AddProductMain from "../components/Products/AddProductMain";
+// import AddProductMain from "../components/Products/AddProductMain";
+// import Orders from "../components/Orders/Orders";
+// import OrderMain from "../components/Orders/OrderMain";
+// import OrderDetailInfo from "../components/Orders/OrderDetailInfo";
+// import OrderDetailProducts from "../components/Orders/OrderDetailProducts";
+import OrderDetailMain from "../components/Orders/OrderDetailMain";
 
 const HomeScreen = () => {
   return (
@@ -13,7 +18,12 @@ const HomeScreen = () => {
         {/* <Header />
         <Main /> */}
         {/* <MainProducts /> */}
-        <AddProductMain />
+        {/* <AddProductMain /> */}
+        {/* <Orders /> */}
+        {/* <OrderMain /> */}
+        {/* <OrderDetailInfo /> */}
+        {/* <OrderDetailProducts /> */}
+        <OrderDetailMain />
       </main>
     </>
   );
