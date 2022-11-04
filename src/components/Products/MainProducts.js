9e7b2bfa1a44275519f343c341f97345df409863
nhokshiroupro <shiroutran@gmail.com>
2022-11-04 10:22:54 +0700

    Integrate API for Get all products

--- src/components/Products/MainProducts.js
+++ src/components/Products/MainProducts.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Product from "./Product";
-import products from "../../data/Products";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../../Redux/Actions/ProductActions";
+import Loading from "../LoadingError/Loading";
+import Message from "../LoadingError/Error";
 
 const MainProducts = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <section className="content-main">
       <div className="content-header">
         <h2 className="content-title">Products</h2>
         <div>
-          <Link to="/addproduct" className="btn btn-primary">Create New</Link>
+          <Link to="/addproduct" className="btn btn-primary">
+            Create New
+          </Link>
         </div>
       </div>
       <div className="card mb-4 shadow-sm">
@@ -25,28 +39,45 @@ const MainProducts = () => {
           </div>
         </header>
         <div className="card-body">
-          <div className="row">
-            {/* Product */}
-            {products.map((product) => (
-              <Product product={product} key={product._id} />
-            ))}
-          </div>
+          {loading ? (
+            <Loading />
+          ) : error ? (
+            <Message variant="alert-danger">{error}</Message>
+          ) : (
+            <div className="row">
+              {/* Product */}
+              {products.map((product) => (
+                <Product product={product} key={product._id} />
+              ))}
+            </div>
+          )}
+
           <nav className="float-end mt-4" aria-label="Page navigation">
             <ul className="pagination">
               <li className="page-item disabled">
-                <Link to="#" className="page-link">Previous</Link>
+                <Link to="#" className="page-link">
+                  Previous
+                </Link>
               </li>
               <li className="page-item active">
-                <Link to="#" className="page-link">1</Link>
+                <Link to="#" className="page-link">
+                  1
+                </Link>
               </li>
               <li className="page-item">
-                <Link to="#" className="page-link">2</Link>
+                <Link to="#" className="page-link">
+                  2
+                </Link>
               </li>
               <li className="page-item">
-                <Link to="#" className="page-link">3</Link>
+                <Link to="#" className="page-link">
+                  3
+                </Link>
               </li>
               <li className="page-item">
-                <Link to="#" className="page-link">Next</Link>
+                <Link to="#" className="page-link">
+                  Next
+                </Link>
               </li>
             </ul>
           </nav>
