90be19cd6714990f7be67e092759bb3709573074
nhokshiroupro <shiroutran@gmail.com>
2022-11-22 17:58:47 +0700

    responsive table

--- src/components/Home/LatestOrder.js
+++ src/components/Home/LatestOrder.js
@@ -5,47 +5,49 @@ const LatestOrder = () => {
   return (
     <div className="card-body">
       <h5 className="card-title">Latest Orders</h5>
-      <table className="table table-striped">
-        <tbody>
-          <tr>
-            <td>
-              <b>Kim Phước</b>
-            </td>
-            <td>kimphuoc@gmail.com</td>
-            <td>100,000 VND</td>
-            <td>
-              <span className="badge rounded-pill alert-success">
-                Paid At Today at 11:11 AM
-              </span>
-            </td>
-            <td>Today at 11:11 AM</td>
-            <td className="d-flex justify-content-end align-item-center">
-              <Link to={`/order`} className="text-success">
-                <i className="fas fa-eye"></i>
-              </Link>
-            </td>
-          </tr>
-          {/* Not Paid */}
-          <tr>
-            <td>
-              <b>Kim Phước</b>
-            </td>
-            <td>kimphuoc@gmail.com</td>
-            <td>100,000 VND</td>
-            <td>
-              <span className="badge rounded-pill alert-danger">
-                Created At Today at 11:11 AM
-              </span>
-            </td>
-            <td>Today at 11:11 AM</td>
-            <td className="d-flex justify-content-end align-item-center">
-              <Link to={`/order`} className="text-success">
-                <i className="fas fa-eye"></i>
-              </Link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="table-responsive">
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>
+                <b>Kim Phước</b>
+              </td>
+              <td>kimphuoc@gmail.com</td>
+              <td>100,000 VND</td>
+              <td>
+                <span className="badge rounded-pill alert-success">
+                  Paid At Today at 11:11 AM
+                </span>
+              </td>
+              <td>Today at 11:11 AM</td>
+              <td className="d-flex justify-content-end align-item-center">
+                <Link to={`/order`} className="text-success">
+                  <i className="fas fa-eye"></i>
+                </Link>
+              </td>
+            </tr>
+            {/* Not Paid */}
+            <tr>
+              <td>
+                <b>Kim Phước</b>
+              </td>
+              <td>kimphuoc@gmail.com</td>
+              <td>100,000 VND</td>
+              <td>
+                <span className="badge rounded-pill alert-danger">
+                  Created At Today at 11:11 AM
+                </span>
+              </td>
+              <td>Today at 11:11 AM</td>
+              <td className="d-flex justify-content-end align-item-center">
+                <Link to={`/order`} className="text-success">
+                  <i className="fas fa-eye"></i>
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/components/Orders/OrderDetailProducts.js
+++ src/components/Orders/OrderDetailProducts.js
@@ -3,62 +3,66 @@ import { Link } from "react-router-dom";
 
 const OrderDetailProducts = () => {
   return (
-    <table className="table border table-lg">
-      <thead>
-        <tr>
-          <th style={{ width: "40%" }}>Product</th>
-          <th style={{ width: "20%" }}>Unit Price</th>
-          <th style={{ width: "20%" }}>Quantity</th>
-          <th style={{ width: "20%" }} className="text-end">
-            Total
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <Link className="itemside" to="#">
-              <div className="left">
-                <img
-                  src="/images/1.png"
-                  alt="prodct"
-                  style={{ width: "40px", height: "40px" }}
-                  className="img-xs"
-                />
-              </div>
-              <div className="info">Vảy Ốc Đỏ{" "}</div>
-            </Link>
-          </td>
-          <td>25,000 VND</td>
-          <td>3</td>
-          <td className="text-end">75,000 VND</td>
-        </tr>
-        <tr>
-          <td colSpan="4">
-            <article className="float-end">
-              <dl className="dlist">
-                <dt>Subtotal:</dt> <dd>123,000 VND</dd>
-              </dl>
-              <dl className="dlist">
-                <dt>Shipping Cost:</dt> <dd>15,000 VND</dd>
-              </dl>
-              <dl className="dlist">
-                <dt>Grand Total:</dt>
-                <dd>
-                  <b className="h5">138,000 VND</b>
-                </dd>
-              </dl>
-              <dl className="dlist">
-                <dt className="text-muted">Status:</dt>
-                <dd>
-                  <span className="badge rounded-pill alert alert-success text-success">Payment Done</span>
-                </dd>
-              </dl>
-            </article>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="table-responsive">
+      <table className="table border table-lg">
+        <thead>
+          <tr>
+            <th style={{ width: "40%" }}>Product</th>
+            <th style={{ width: "20%" }}>Unit Price</th>
+            <th style={{ width: "20%" }}>Quantity</th>
+            <th style={{ width: "20%" }} className="text-end">
+              Total
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Link className="itemside" to="#">
+                <div className="left">
+                  <img
+                    src="/images/1.png"
+                    alt="prodct"
+                    style={{ width: "40px", height: "40px" }}
+                    className="img-xs"
+                  />
+                </div>
+                <div className="info">Vảy Ốc Đỏ </div>
+              </Link>
+            </td>
+            <td>25,000 VND</td>
+            <td>3</td>
+            <td className="text-end">75,000 VND</td>
+          </tr>
+          <tr>
+            <td colSpan="4">
+              <article className="float-end">
+                <dl className="dlist">
+                  <dt>Subtotal:</dt> <dd>123,000 VND</dd>
+                </dl>
+                <dl className="dlist">
+                  <dt>Shipping Cost:</dt> <dd>15,000 VND</dd>
+                </dl>
+                <dl className="dlist">
+                  <dt>Grand Total:</dt>
+                  <dd>
+                    <b className="h5">138,000 VND</b>
+                  </dd>
+                </dl>
+                <dl className="dlist">
+                  <dt className="text-muted">Status:</dt>
+                  <dd>
+                    <span className="badge rounded-pill alert alert-success text-success">
+                      Payment Done
+                    </span>
+                  </dd>
+                </dl>
+              </article>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 export default OrderDetailProducts;
